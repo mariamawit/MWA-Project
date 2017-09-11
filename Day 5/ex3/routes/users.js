550ead5d414cb160e3fc2d78f9e684b580fa10d8
Mariamawit <mawitey34@gmail.com>
2017-09-11 15:54:46 -0500

    modified

--- Day 5/ex3/routes/users.js
+++ Day 5/ex3/routes/users.js
@@ -3,29 +3,24 @@ var fetch = require('node-fetch');
 var Rx = require('rxjs');
 var router = express.Router();
 
-const getsUsersAsync = async function(){
+const getsUsersAsync = async function(res){
   let data;
   try{
-    data = await fetch('http://jsonplaceholder.typicode.com/users/');
+    data = await fetch('http://jsonplaceholder.typicode.com/users/').then(d => { return d.json() });
+   res.render('users', { userdata: data});
   }
   catch(err){
-
+    console.log(err)
   }
-  return data || '';
 }
 
 router.get('/async', function(req, res, next){
-  getsUsersAsync()
-  .then(d => { return d.json() })
-  .then(d => res.render('users', { userdata: d}))
-  .catch(err => console.log(err));
-});
+  getsUsersAsync(res)
+  });
 
 
 router.get('/rxjs', function(req, res, next){
-  Rx.Observable.fromPromise(fetch('http://jsonplaceholder.typicode.com/users/')
-    .then(d => { return d.json() })
-    .catch(err => console.log(err)))
+  Rx.Observable.fromPromise(fetch('http://jsonplaceholder.typicode.com/users/')).flatMap(data=>data.json())
     .subscribe(
       d => res.render('users', { userdata: d}),
       err => console.log(err),
